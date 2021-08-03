9cd2d062629b46159af2f2d1073cd6c4f1277a76
Akshaykumar Kanani <67008993+akshaykanani9500@users.noreply.github.com>
2021-08-04 02:21:04 +0530

    Update settings.py

--- crud_Django/crud_Django/settings.py
+++ crud_Django/crud_Django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_j)r$@e=_sekf&_i%gu!(!sz&d!wa78v&y14wh(g=i*izd$3&h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
